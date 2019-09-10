bbce4215c10f56eed99ba4256ce7bbc508acf830
Kainan Liu <liuknan@bu.edu>
2019-09-09 23:43:21 -0400

    add features

--- leisure/museum.py
+++ leisure/museum.py
@@ -1,19 +1,59 @@
-import sys
-def draw_museum(In):
-    if In not in ["Vertical","Horizontal"]:   
-        sys.exit()
+import math
+
+
+def draw_museum(direction, length):
+    museum = ["M", "u", "s", "e", "u", "m"]
+    if direction not in ["Vertical", "Horizontal"]:
+        raise ValueError
+    if length <= 0:
+        raise ValueError
     else:
-        if In == "Horizontal":
-            print(" ------ ")
-            print("|Museum|")
-            print(" ------ ")
+        length = math.ceil(length)
+        if direction == "Horizontal":
+            print(" ", end="")
+            for _ in range(length):
+                print("-", end="")
+            print(" ")
+            print("|", end="")
+            if length > 6:
+                start = length // 2 - 3
+                end = start + 5
+                num = 0
+                for i in range(length):
+                    if start <= i <= end:
+                        print(museum[num], end="")
+                        num += 1
+                    else:
+                        print(" ", end="")
+            else:
+                for i in range(length):
+                    print(museum[i], end="")
+            print("|")
+            print(" ", end="")
+            for _ in range(length):
+                print("-", end="")
+            print(" ")
         else:
-            print(" --- ")
-            print("| M |")
-            print("| u |")
-            print("| s |")
-            print("| e |")
-            print("| u |")
-            print("| m |")
-            print(" --- ")
-    return 
+            if length > 6:
+                start = length // 2 - 3
+                end = start + 5
+                num = 0
+                print(" --- ")
+                for i in range(length):
+                    if start <= i <= end:
+                        print(" |", end="")
+                        print(museum[num], end="")
+                        num += 1
+                        print("|")
+                    else:
+                        print(" | |")
+                print(" --- ")
+            else:
+                print(" --- ")
+                for i in range(length):
+                    print("|"+museum[i]+"|")
+                print(" --- ")
+    return
+
+
+draw_museum("Vertical", 10)
