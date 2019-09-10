2c37f8d3e81a6fc0a14e75267949665040d5f16c
Kainan Liu <liuknan@bu.edu>
2019-09-09 23:05:15 -0400

    add museum

--- leisure/museum.py
+++ leisure/museum.py
@@ -1,6 +1,19 @@
-def draw_museum():
-    print(" ------ ")
-    print("|Museum|")
-    print(" ------ ")
-
-    return
+import sys
+def draw_museum(In):
+    if In not in ["Vertical","Horizontal"]:   
+        sys.exit()
+    else:
+        if In == "Horizontal":
+            print(" ------ ")
+            print("|Museum|")
+            print(" ------ ")
+        else:
+            print(" --- ")
+            print("| M |")
+            print("| u |")
+            print("| s |")
+            print("| e |")
+            print("| u |")
+            print("| m |")
+            print(" --- ")
+    return 
